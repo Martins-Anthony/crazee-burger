036cc2a45cfeec69ff8e8fbfdbcaf563d9e58b77
Martins <thony.martins.67@gmail.com>
2025-10-04 23:59:51 +0200

    ✨ F01: LOGIN > create LoginPage and LoginForm

---
+++ src/components/pages/login/LoginForm.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+export default function LoginForm() {
+  const [inputValue, setInputValue] = useState("") 
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setInputValue(event.target[0].value)
+    alert("Bonjour " + inputValue)
+    setInputValue("")
+  }
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value)
+  }
+
+  return (
+    <>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input type="text" placeholder="Entre votre prénom..." required value={inputValue} onChange={handleChange}/>
+        <button type="submit">Accédez à votre espace</button>
+      </form>
+    </>
+  )
+}
