bc8541858d03c2ed3fd3dc76f54930e5e2c8c070
Martins <thony.martins.67@gmail.com>
2025-10-12 03:34:40 +0200

    ✨ F03: LOGIN > add css and background for loginPage

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,11 +1,42 @@
 import Logo from '../../Logo'
 import LoginForm from './LoginForm'
+import backgroundBurger from '../../../assets/burger-background.jpg'
+import styled from 'styled-components'
 
 export default function LoginPage() {
   return (
-    <>
-      <Logo />
-      <LoginForm />
-    </>
+    <LoginPageStyled>
+      <div className="container">
+        <Logo />
+        <LoginForm />
+      </div>
+    </LoginPageStyled>
   )
 }
+
+const LoginPageStyled = styled.div`
+  width: 1512px;
+  height: 982px;
+  position: relative;
+  background-image: url(${backgroundBurger});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  &::before {
+    content: '';
+    position: absolute;
+    inset: 0;
+    background: rgba(0, 0, 0, 0.55);
+    pointer-events: none;
+  }
+
+  > * {
+    position: relative;
+    z-index: 1;
+  }
+
+  .container {
+    padding-top: 200px;
+  }
+`
