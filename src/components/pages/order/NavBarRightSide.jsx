d5456e5e574d82a27f028edf747aa9c45a0963c0
Martins <thony.martins.67@gmail.com>
2025-10-18 21:50:47 +0200

    ♻️  F04: ORDER > NavBar > add Profile

--- src/components/pages/order/NavBarRightSide.jsx
+++ src/components/pages/order/NavBarRightSide.jsx
@@ -1,58 +1,15 @@
-import { IoPersonCircleOutline } from 'react-icons/io5'
-import { Link } from 'react-router'
 import styled from 'styled-components'
 import { theme } from '../../../theme'
+import Profile from './Profile'
 
 export default function NavBarRightSide({ username }) {
   return (
     <NavBarRightSideStyled>
-      <div className="card-user">
-        <h4>
-          Hey, <span className="user">{username}</span>
-        </h4>
-        <Link to={'/'}>
-          <button className="logout">Se déconnexion</button>
-        </Link>
-      </div>
-      <IoPersonCircleOutline />
+      <Profile username={username} />
     </NavBarRightSideStyled>
   )
 }
 
 const NavBarRightSideStyled = styled.div`
-  display: flex;
-  gap: ${theme.spacing.sm};
-
-  .card-user {
-    color: ${theme.colors.greyBlue};
-
-    h4 {
-      font-weight: ${theme.fonts.weights.regular};
-      margin: 0;
-    }
-
-    .user {
-      color: ${theme.colors.primary};
-      font-weight: ${theme.fonts.weights.bold};
-    }
-  }
-
-  svg {
-    font-size: ${theme.fonts.size.P4};
-    color: ${theme.colors.greyBlue};
-  }
-
-  .logout {
-    font-size: ${theme.fonts.size.XXS};
-    font-weight: ${theme.fonts.weights.regular};
-    color: ${theme.colors.greyBlue};
-    padding: 0;
-    border: none;
-    background: none;
-    &:hover {
-      cursor: pointer;
-      text-decoration: underline;
-      text-underline-offset: 0.25rem;
-    }
-  }
+  margin-right: ${theme.spacing.lg};
 `
---
+++ src/components/pages/order/Profile.jsx
@@ -0,0 +1,58 @@
+import { IoPersonCircleOutline } from 'react-icons/io5'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { theme } from '../../../theme'
+
+export default function Profile({ username }) {
+  return (
+    <ProfileStyled>
+      <div className="info">
+        <h4>
+          Hey, <b className="user">{username}</b>
+        </h4>
+        <Link to={'/'}>
+          <button className="logout">Se déconnexion</button>
+        </Link>
+      </div>
+      <IoPersonCircleOutline className="picture" />
+    </ProfileStyled>
+  )
+}
+
+const ProfileStyled = styled.div`
+  display: flex;
+  gap: ${theme.spacing.sm};
+
+  .info {
+    color: ${theme.colors.greyBlue};
+    text-align: right;
+
+    h4 {
+      font-weight: ${theme.fonts.weights.regular};
+      margin: 0;
+    }
+
+    .user {
+      color: ${theme.colors.primary};
+    }
+  }
+
+  .picture {
+    font-size: ${theme.fonts.size.P4};
+    color: ${theme.colors.greyBlue};
+  }
+
+  .logout {
+    font-size: ${theme.fonts.size.XXS};
+    font-weight: ${theme.fonts.weights.regular};
+    color: ${theme.colors.greyBlue};
+    padding: 0;
+    border: none;
+    background: none;
+    &:hover {
+      cursor: pointer;
+      text-decoration: underline;
+      text-underline-offset: 0.25rem;
+    }
+  }
+`
