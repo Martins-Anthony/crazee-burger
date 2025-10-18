df7ace4d0bd11049d8f1752fed4d3d7d1cee148c
Martins <thony.martins.67@gmail.com>
2025-10-19 00:44:49 +0200

    📦  F05: images > add pictures

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,6 +1,5 @@
 import Logo from '../../reusable-ui/Logo'
 import LoginForm from './LoginForm'
-import backgroundBurger from '../../../assets/burger-background.jpg'
 import styled from 'styled-components'
 
 export default function LoginPage() {
@@ -23,7 +22,7 @@ const LoginPageStyled = styled.div`
     content: '';
     position: absolute;
     inset: 0;
-    background: url(${backgroundBurger}) rgba(0, 0, 0, 0.7);
+    background: url(${'/images/burger-and-fries-background.jpg'}) rgba(0, 0, 0, 0.7);
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
