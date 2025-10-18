e0b3e10b759ebfeeb5f1975e5e3873b76d00ed15
Martins <thony.martins.67@gmail.com>
2025-10-18 13:02:17 +0200

    ♻️  F04: ORDER > NavBar > create NavBarRightSide

--- src/components/pages/order/NavBar.jsx
+++ src/components/pages/order/NavBar.jsx
@@ -1,23 +1,12 @@
-import { IoPersonCircleOutline } from 'react-icons/io5'
-import { Link } from 'react-router'
 import styled from 'styled-components'
 import { theme } from '../../../theme'
+import NavBarRightSide from './NavBarRightSide'
 
 export default function NavBar({ username }) {
   return (
     <NavBarStyled>
       <div>logo</div>
-      <div className="container-card-user">
-        <div className="card-user">
-          <h4>
-            Hey, <span className="user">{username}</span>
-          </h4>
-          <Link to={'/'}>
-            <button className="logout">Se déconnexion</button>
-          </Link>
-        </div>
-        <IoPersonCircleOutline />
-      </div>
+      <NavBarRightSide username={username} />
     </NavBarStyled>
   )
 }
@@ -31,44 +20,5 @@ const NavBarStyled = styled.nav`
   padding-left: ${theme.spacing.md};
   padding-right: ${theme.spacing.xl};
   background: ${theme.colors.white};
-
-  .container-card-user {
-    display: flex;
-    gap: ${theme.spacing.sm};
-
-    .card-user {
-      font-family: ${theme.fonts.family.secondary};
-      color: ${theme.colors.greyBlue};
-
-      h4 {
-        font-weight: ${theme.fonts.weights.regular};
-        margin: 0;
-      }
-
-      .user {
-        color: ${theme.colors.primary};
-        font-weight: ${theme.fonts.weights.bold};
-      }
-    }
-
-    svg {
-      font-size: ${theme.fonts.size.P4};
-      color: ${theme.colors.greyBlue};
-    }
-  }
-
-  .logout {
-    font-size: ${theme.fonts.size.XXS};
-    font-family: ${theme.fonts.family.secondary};
-    font-weight: ${theme.fonts.weights.regular};
-    color: ${theme.colors.greyBlue};
-    padding: 0;
-    border: none;
-    background: none;
-    &:hover {
-      cursor: pointer;
-      text-decoration: underline;
-      text-underline-offset: 0.25rem;
-    }
-  }
+  border-radius: ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound} 0 0;
 `
