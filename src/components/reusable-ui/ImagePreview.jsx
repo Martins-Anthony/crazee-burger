5fce4a9d871532ea43569f46b4c2859bd534563a
Martins <thony.martins.67@gmail.com>
2025-11-09 00:15:36 +0100

    💄  F08: ADMIN > ImagePreview > add UI

--- src/components/reusable-ui/ImagePreview.jsx
+++ src/components/reusable-ui/ImagePreview.jsx
@@ -1,19 +1,37 @@
 import styled from 'styled-components'
 import { theme } from '../../theme'
 
-export default function ImagePreview() {
-  return <ImagePreviewStyled className="image-preview">Aucune image</ImagePreviewStyled>
+export default function ImagePreview({ url }) {
+  return (
+    <ImagePreviewStyled>
+      {url.length > 0 ? <img src={url} alt="Image-preview" /> : <div>Aucune image</div>}
+    </ImagePreviewStyled>
+  )
 }
 
 const ImagePreviewStyled = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  height: 7.5rem;
-
-  border: 1px solid ${theme.colors.greyLight};
-  border-radius: ${theme.borderRadius.round};
-
   color: ${theme.colors.greySemiDark};
+  grid-row: span 3 / span 3;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  grid-template-columns: 1fr;
+
+  div {
+    display: grid;
+    justify-content: center;
+    align-items: center;
+
+    height: 100%;
+    width: 100%;
+
+    border: 1px solid ${theme.colors.greyLight};
+    border-radius: ${theme.borderRadius.round};
+  }
+
+  img {
+    width: 100px;
+    height: 100px;
+    margin: 0 auto;
+  }
 `
